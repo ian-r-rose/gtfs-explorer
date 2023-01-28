935d6fccb10270b9427584022ed40108e1b46e39
Ian Rose <ian.r.rose@gmail.com>
2023-01-27 19:07:51 -0800

    Get esbuild working.

--- build.mjs
+++ build.mjs
@@ -1,7 +1,43 @@
-import * as esbuild from "esbuild";
+import esbuild from "esbuild";
+import fs from "fs";
+import path from "path";
+import { createRequire } from "module";
+
+const require = createRequire(import.meta.url);
+const DUCKDB_DIST = path.dirname(require.resolve("@duckdb/duckdb-wasm"));
+
+function printErr(err) {
+  if (err) return console.log(err);
+}
+
+fs.copyFile(
+  path.resolve(DUCKDB_DIST, "duckdb-mvp.wasm"),
+  "./public/duckdb-mvp.wasm",
+  printErr
+);
+fs.copyFile(
+  path.resolve(DUCKDB_DIST, "duckdb-eh.wasm"),
+  "./public/duckdb-eh.wasm",
+  printErr
+);
+fs.copyFile(
+  path.resolve(DUCKDB_DIST, "duckdb-browser-mvp.worker.js"),
+  "./public/duckdb-browser-mvp.worker.js",
+  printErr
+);
+fs.copyFile(
+  path.resolve(DUCKDB_DIST, "duckdb-browser-eh.worker.js"),
+  "./public/duckdb-browser-eh.worker.js",
+  printErr
+);
 
 await esbuild.build({
-  entryPoints: ["./src/index.tsx"],
-  bundle: true,
+  entryPoints: ["src/index.tsx"],
   outdir: "public",
+  platform: "browser",
+  format: "iife",
+  target: "esnext",
+  bundle: true,
+  minify: false,
+  sourcemap: false,
 });
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,42 @@ import React from "react";
 
 import App from "./app";
 
+import * as duckdb from "@duckdb/duckdb-wasm";
+import * as arrow from "apache-arrow";
+
+(async () => {
+  try {
+    const DUCKDB_CONFIG = await duckdb.selectBundle({
+      mvp: {
+        mainModule: "./duckdb-mvp.wasm",
+        mainWorker: "./duckdb-browser-mvp.worker.js",
+      },
+      eh: {
+        mainModule: "./duckdb-eh.wasm",
+        mainWorker: "./duckdb-browser-eh.worker.js",
+      },
+    });
+
+    const logger = new duckdb.ConsoleLogger();
+    const worker = new Worker(DUCKDB_CONFIG.mainWorker!);
+    const db = new duckdb.AsyncDuckDB(logger, worker);
+    await db.instantiate(DUCKDB_CONFIG.mainModule, DUCKDB_CONFIG.pthreadWorker);
+
+    const conn = await db.connect();
+    const res = await conn.query<{ v: arrow.Int }>(
+      `SELECT count(*)::INTEGER as v FROM generate_series(0, 100) t(v)`
+    );
+    console.log(res);
+
+    await conn.close();
+    await db.terminate();
+    worker.terminate();
+  } catch (e) {
+    console.error(e);
+  }
+})();
+
 const container = document.getElementById("root");
 const root = createRoot(container!);
+
 root.render(<App />);
