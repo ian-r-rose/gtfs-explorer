74b760803763c5b462def1727d306e9b06d6f132
Ian Rose <ian.r.rose@gmail.com>
2023-02-01 08:30:08 -0800

    Quck and dirty SQL input form

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,91 @@
 import React from "react";
+import * as duckdb from "@duckdb/duckdb-wasm";
+import { initializeDuckDB } from "./duckdb";
+import { loadGTFS } from "./load";
 
 export default function App() {
-  return <div>Hello World!</div>;
+  const [conn, setConn] = React.useState<duckdb.AsyncDuckDBConnection | null>(
+    null
+  );
+
+  React.useEffect(() => {
+    const init = async () => {
+      const db = await initializeDuckDB();
+      await loadGTFS(db);
+      const conn = await db.connect();
+      setConn(conn);
+    };
+    init();
+  }, []);
+  return <SQLForm conn={conn} />;
 }
+
+type SQLFormProps = {
+  conn: duckdb.AsyncDuckDBConnection | null;
+};
+
+type SQLInputProps = {
+  onSubmit: (value: string) => void;
+};
+
+type SQLOutputProps = {
+  result: string;
+};
+
+const SQLInput = (props: SQLInputProps) => {
+  const [value, setValue] = React.useState("");
+
+  const onChange = (evt: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setValue(evt.target.value);
+  };
+
+  const onKeyDown = (evt: React.KeyboardEvent) => {
+    if (evt.shiftKey && evt.key === "Enter") {
+      evt.stopPropagation();
+      evt.preventDefault();
+      props.onSubmit(value);
+    }
+  };
+
+  return (
+    <textarea
+      value={value}
+      onChange={onChange}
+      onKeyDown={onKeyDown}
+      style={{ height: "480px", width: "640px" }}
+    ></textarea>
+  );
+};
+
+const SQLOutput = (props: SQLOutputProps) => {
+  return (
+    <pre style={{ height: "480px", width: "640px" }}>
+      <code>{props.result}</code>
+    </pre>
+  );
+};
+
+const SQLForm = (props: SQLFormProps) => {
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (value: string) => {
+    if (!props.conn) {
+      return;
+    }
+    const table = await props.conn.query(value);
+    setResult(table.toString());
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-around",
+      }}
+    >
+      <SQLInput onSubmit={onSubmit} />
+      <SQLOutput result={result} />
+    </div>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -6,14 +6,35 @@ import App from "./app";
 import { initializeDuckDB } from "./duckdb";
 import { loadGTFS } from "./load";
 
-(async () => {
+async () => {
   const db = await initializeDuckDB();
-  await loadGTFS(db);
+  // await loadGTFS(db);
 
   const conn = await db.connect();
-  const results = await conn.query("SELECT * FROM stop_times LIMIT 10");
-  console.log(results);
-})();
+
+  const results = await conn.query(`
+    WITH most_common_shapes AS (
+      SELECT route_id, mode(shape_id) as shape_id
+      FROM trips
+      GROUP BY route_id
+    ),
+    shape_by_route AS (
+        SELECT
+          most_common_shapes.route_id,
+          shapes.shape_id,
+          shapes.shape_pt_lon,
+          shapes.shape_pt_lat
+        FROM most_common_shapes
+        INNER JOIN shapes ON shapes.shape_id = most_common_shapes.shape_id
+        ORDER BY most_common_shapes.route_id, shapes.shape_pt_sequence
+    )
+    SELECT route_id, list(list_pack(shape_pt_lon, shape_pt_lat)) AS lonlat
+    FROM shape_by_route
+    GROUP BY route_id
+    `);
+  console.log(results.toString());
+  // window.result = results;
+};
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
